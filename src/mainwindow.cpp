46f875ef7342003324bdcb4c62fdbe6e7afd7fa5
markuspg <markuspg@users.noreply.github.com>
2019-01-01 12:44:53 +0100

    Add all bible book names to QComboBox

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -26,13 +26,156 @@
 #include <QPushButton>
 #include <QStandardPaths>
 
+enum EBookNames {
+    GEN,
+    EX,
+    LEV,
+    NUM,
+    DEUT,
+    JOSH,
+    JUDG,
+    RUTH,
+    FRST_SAM,
+    SCND_SAM,
+    FRST_KINGS,
+    SCND_KINGS,
+    FRST_CHRON,
+    SCND_CHRON,
+    EZRA,
+    NEH,
+    EST,
+    JOB,
+    PS,
+    PROV,
+    ECCLES,
+    SONG,
+    ISA,
+    JER,
+    LAM,
+    EZEK,
+    DAN,
+    HOS,
+    JOEL,
+    AMOS,
+    OBAD,
+    JONAH,
+    MIC,
+    NAH,
+    HAB,
+    ZEPH,
+    HAG,
+    ZECH,
+    MAL,
+    MATT,
+    MARK,
+    LUKE,
+    JOHN,
+    ACTS,
+    ROM,
+    FRST_COR,
+    SCND_COR,
+    GAL,
+    EPH,
+    PHIL,
+    COL,
+    FRST_THESS,
+    SCND_THESS,
+    FRST_TIM,
+    SCND_TIM,
+    TITUS,
+    PHILEM,
+    HEB,
+    JAMES,
+    FRST_PET,
+    SCND_PET,
+    FRST_JOHN,
+    SCND_JOHN,
+    THRD_JOHN,
+    JUDE,
+    REV,
+
+    ZZZ_BIBLE_BOOK_QTY,
+};
+
+static const std::vector<QString> bookNames {
+    "Genesis",          // GEN
+    "Exodus",           // EX
+    "Leviticus",        // LEV
+    "Numbers",          // NUM
+    "Deutoronomy",      // DEUT
+    "Joshua",           // JOSH
+    "Judges",           // JUDG
+    "Ruth",             // RUTH
+    "1 Samuel",         // FRST_SAM
+    "2 Samuel",         // SCND_SAM
+    "1 Kings",          // FRST_KINGS
+    "2 Kings",          // SCND_KINGS
+    "1 Chronicles",     // FRST_CHRON
+    "2 Chronicles",     // SCND_CHRON
+    "Ezra",             // EZRA
+    "Nehemiah",         // NEH
+    "Esther",           // EST
+    "Job",              // JOB
+    "Psalms",           // PS
+    "Proverbs",         // PROV
+    "Ecclesiastes",     // ECCLES
+    "Song of Solomon",  // SONG
+    "Isaiah",           // ISA
+    "Jeremiah",         // JER
+    "Lamentations",     // LAM
+    "Ezekiel",          // EZEK
+    "Daniel",           // DAN
+    "Hosea",            // HOS
+    "Joel",             // JOEL
+    "Amos",             // AMOS
+    "Obadiah",          // OBAD
+    "Jonah",            // JONAH
+    "Micah",            // MIC
+    "Nahum",            // NAH
+    "Habakkuk",         // HAB
+    "Zephaniah",        // ZEPH
+    "Haggai",           // HAG
+    "Zechariah",        // ZECH
+    "Malachi",          // MAL
+    "Matthew",          // MATT
+    "Mark",             // MARK
+    "Luke",             // LUKE
+    "John",             // JOHN
+    "Acts",             // ACTS
+    "Romans",           // ROM
+    "1 Corinthians",    // FRST_COR
+    "2 Corinthians",    // SCND_COR
+    "Galatians",        // GAL
+    "Ephesians",        // EPH
+    "Philippians",      // PHIL
+    "Colossians",       // COL
+    "1 Thessalonians",  // FRST_THESS
+    "2 Thessalonians",  // SCND_THESS
+    "1 Timothy",        // FRST_TIM
+    "2 Timothy",        // SCND_TIM
+    "Titus",            // TITUS
+    "Philemon",         // PHILEM
+    "Hebrews",          // HEB
+    "James",            // JAMES
+    "1 Peter",          // FRST_PET
+    "2 Peter",          // SCND_PET
+    "1 John",           // FRST_JOHN
+    "2 John",           // SCND_JOHN
+    "3 John",           // THRD_JOHN
+    "Jude",             // JUDE
+    "Revelation",       // REV
+};
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
-    ui->CBBibleBook->addItems(QStringList(QStringList() << "Gen" << "Ex" << "Lev"));
+    for (int i = 0; i < ZZZ_BIBLE_BOOK_QTY; ++i) {
+        ui->CBBibleBook->addItem(bookNames.at(
+            static_cast<std::vector<QString>::size_type>(i)));
+    }
 
     connect(ui->PBSave, SIGNAL(clicked(bool)),
             this, SLOT(SaveData()));
